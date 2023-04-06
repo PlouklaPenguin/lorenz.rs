6e1205fbb5a37ee1b30f93ad2a2220739fbfe079
Sam Robinson <62719122+PlouklaPenguin@users.noreply.github.com>
2023-04-06 13:31:57 -0700

    spitted straight bars for the params

--- src/main.rs
+++ src/main.rs
@@ -31,17 +31,32 @@ fn draw_chaos(loc: [(f32, f32, f32); POINTS], canvas: &mut Canvas<Window>) -> Re
 
 struct Slider<'a> {
     text: String,
-    size: u32,
+    sw: u32,
+    sh: u32,
+    bw: u32,
+    bh: u32,
     x: i32,
     y: i32,
     value: &'a mut f32,
 }
 
 impl<'a> Slider<'a> {
-    fn new(text: String, size: u32, x: i32, y: i32, value: &'a mut f32) -> Slider {
+    fn new(
+        text: String,
+        sw: u32,
+        sh: u32,
+        bw: u32,
+        bh: u32,
+        x: i32,
+        y: i32,
+        value: &'a mut f32,
+    ) -> Slider {
         Slider {
             text,
-            size,
+            sw,
+            sh,
+            bw,
+            bh,
             x,
             y,
             value,
@@ -49,8 +64,15 @@ impl<'a> Slider<'a> {
     }
 
     fn draw(&self, canvas: &mut Canvas<Window>) -> Result<(), String> {
-        let rect = Rect::new(self.x, self.y, self.size, 4);
-        canvas.draw_rect(rect)?;
+        let rect = Rect::new(self.x, self.y, self.sw, self.sh);
+        let bar = Rect::new(
+            self.x + (*self.value) as i32,
+            self.y,
+            self.bw,
+            self.bh,
+        );
+        canvas.fill_rect(rect)?;
+        canvas.fill_rect(bar)?;
         //canvas.draw_line(self.x, (self.position.0 + self.size as i32 * 100, self.position.1))?;
         Ok(())
     }
@@ -89,8 +111,9 @@ fn main() -> Result<(), String> {
     let mut beta = 13_f32 / 3_f32;
     let mut rho = 52_f32;
 
-    let sigma_button = Slider::new(String::from("Sigma"), 40, 4, 4, &mut sigma);
-    
+    let sigma_button = Slider::new(String::from("Sigma"), 100, 4, 2, 10, 15, 15, &mut sigma);
+    let beta_button = Slider::new(String::from("Sigma"), 100, 4, 2, 10, 15, 65, &mut beta);
+    let rho_button = Slider::new(String::from("Sigma"), 100, 4, 2, 10, 15, 115, &mut rho);
     
 
     'running: loop {
@@ -98,6 +121,22 @@ fn main() -> Result<(), String> {
 
         for event in event_pump.poll_iter() {
             match event {
+                Event::MouseMotion { mousestate, x, y, .. } => {
+                    if mousestate.left() {
+                        match (x, y) {
+                            (15..=115, 15..=35) => {
+                                *sigma_button.value = (x - sigma_button.x) as f32;
+                            },
+                            (15..=115, 65..=85) => {
+                                *beta_button.value = (x - sigma_button.x) as f32;
+                            },
+                            (15..=115, 115..=135) => {
+                                *rho_button.value = (x - sigma_button.x) as f32;
+                            },
+                            _ => ()
+                        }
+                    } 
+                },
                 Event::Quit { .. }
                 | Event::KeyDown {
                     keycode: Some(Keycode::Escape),
@@ -106,25 +145,30 @@ fn main() -> Result<(), String> {
                 _ => (),
             }
         }
+        
+    
 
-        // canvas.set_draw_color(Color::RGB(0, 0, 0));
-        // canvas.clear();
+        canvas.set_draw_color(Color::RGB(0, 0, 0));
+        canvas.clear();
 
         canvas.set_draw_color(Color::RGB(255, 255, 255));
         draw_chaos(loc, &mut canvas)?;
-        
+
         sigma_button.draw(&mut canvas)?;
+        rho_button.draw(&mut canvas)?;
+        beta_button.draw(&mut canvas)?;
 
         loc = loc.map(|(mut x, mut y, mut z)| {
             x += (*sigma_button.value * (y - x)) * 0.01;
-            y += (x * (rho - z) - y) * 0.01;
-            z += ((x * y) - (beta * z)) * 0.01;
+            y += (x * (*rho_button.value - z) - y) * 0.01;
+            z += ((x * y) - (*beta_button.value * z)) * 0.01;
             (x, y, z)
         });
-
+        
+        println!("S: {}, R: {}, B: {}", *sigma_button.value, *rho_button.value, *beta_button.value);
+        
         canvas.set_draw_color(Color::RGB(100, 255, 2));
         draw_chaos(loc, &mut canvas)?;
-        
 
         //println!("x: {}, y: {}", x, y);
         canvas.present();
